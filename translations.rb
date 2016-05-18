02412342ddf05eb01811228cf088724890a53369
Mike Kavouras <kavourasm@gmail.com>
2016-05-17 22:21:10 -0500

    add files changed

--- translations.rb
+++ translations.rb
@@ -12,12 +12,12 @@ post '/payload' do
   pr = fetch_and_parse_json
   diff = run_diff(pr)
   locales = diff.select { |d| d.include? $locales_path }
-  comment(message) unless locales.empty?
+  post_comment(pr, locales) unless locales.empty?
 
   status 200
 end
 
-def comment(message)
+def post_comment(pr, locales)
   messages = [
     "ci sono nuove stringhe que sono in disperato bisogno di traduzione",
     "hay nuevas cadenas que están en desesperada necesidad de la traducción",
@@ -25,7 +25,8 @@ def comment(message)
     "il y a des nouvelles chaînes Que sont dans le besoin désespéré de la traduction",
     "gibt es neue Saiten que sind in einer verzweifelten Notwendigkeit Übersetzung"
   ]
-  message = "@mikekavouras: #{messages.sample}"
+  message = "@mikekavouras: #{messages.sample}: "
+  message += locales.map { |locale| "`#{locale}`" }.join(' ')
   `curl "https://api.github.com/repos/teespring/rails-teespring/issues/#{pr["number"]}/comments" -u #{ENV['USERNAME']}:#{ENV['TOKEN']} -d '{"body":"#{message}"}'`
 end
 
